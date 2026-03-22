b34e29a2edf531d41de14f5dc182cf11e5a62d2b
TundraClimate <tundra.tundere@gmail.com>
2026-03-22 16:55:48 +0900

    ✨feat: Create uploader

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use clap::Parser;
 
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::thread;
+use std::time::Duration;
+
+use steamworks::{AppId, Client, PublishedFileId};
+
 use lor_mod_uploader::UpdateEntry;
 use lor_mod_uploader::args::Args;
 
@@ -37,4 +44,47 @@ async fn main() {
 
         return;
     };
+
+    let Ok(client) = Client::init() else {
+        eprintln!("Steam client not launched");
+
+        return;
+    };
+
+    let ugc = client.ugc();
+    let wait = Arc::new(AtomicBool::new(false));
+
+    let item_id = option_env!("WORKSHOP_ID")
+        .and_then(|tid| tid.parse::<u64>().ok())
+        .unwrap_or(0);
+
+    let wait_cb = wait.clone();
+
+    let _handle = ugc
+        .start_item_update(AppId(LOR_ID), PublishedFileId(item_id))
+        .title(entry.title)
+        .description(entry.desc)
+        .preview_path(&entry.thumbnail_path)
+        .content_path(&entry.content_path)
+        .visibility(entry.visibility)
+        .tags(entry.tags, false)
+        .submit(Some(""), move |upload_result| {
+            match upload_result {
+                Ok((_, needs_agree)) if needs_agree => eprintln!("Needs agree to terms"),
+                Ok((published_id, _)) => println!("Uploaded item with id {:?}", published_id),
+                Err(e) => eprintln!("Error uploading item: {:?}", e),
+            }
+
+            wait_cb.store(true, Ordering::SeqCst);
+        });
+
+    loop {
+        if wait.load(Ordering::SeqCst) {
+            break;
+        }
+
+        client.run_callbacks();
+
+        thread::sleep(Duration::from_millis(100));
+    }
 }
